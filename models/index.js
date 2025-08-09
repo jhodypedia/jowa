991ed7d202d7178c626c36862def962b627640fa
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-08-09 13:40:56 +0700

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,9 @@
+// models/index.js
+import sequelize from "../config/database.js";
+import User from "./User.js";
+import Message from "./Message.js";
+import Contact from "./Contact.js";
+import Log from "./Log.js";
+
+const db = { sequelize, User, Message, Contact, Log };
+export default db;
