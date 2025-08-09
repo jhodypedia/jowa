44d36b3bf3a36b83c2d0e68cb79369ab56f5f014
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-08-09 13:43:38 +0700

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+// routes/auth.js
+import express from "express";
+import { register, login, me } from "../controllers/authController.js";
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", me);
+
+export default router;
