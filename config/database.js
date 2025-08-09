48341ec1f16581c3fafc5923697c36b74a206235
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-08-09 08:02:38 +0000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "wa_db",
+  process.env.DB_NAME || "whatsapp_panel",
   process.env.DB_USER || "root",
   process.env.DB_PASS || "",
   {
