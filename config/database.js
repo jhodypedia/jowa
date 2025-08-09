4d51f017c330a9845ecb94e972a0f9740e2de051
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-08-09 15:36:02 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "whatsapp_panel",
-  process.env.DB_USER || "root",
-  process.env.DB_PASS || "",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: Number(process.env.DB_PORT || 3306),
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
     logging: false,
     pool: { max: 10, min: 0, acquire: 30000, idle: 10000 }
