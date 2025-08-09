97a323360bf135a579e0cfe3ce7eb14f4e552a67
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-08-09 19:43:28 +0700

    Update Contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -1,14 +1,21 @@
-// models/Contact.js
-import { DataTypes } from "sequelize";
-import sequelize from "../config/database.js";
+export default (sequelize, DataTypes) => {
+  const Contact = sequelize.define("Contact", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    tableName: "contacts"
+  });
 
-const Contact = sequelize.define("Contact", {
-  waId: { type: DataTypes.STRING, allowNull: false, unique: true },
-  name: { type: DataTypes.STRING, allowNull: true },
-  raw: { type: DataTypes.JSON, allowNull: true }
-}, {
-  tableName: "contacts",
-  timestamps: false
-});
-
-export default Contact;
+  return Contact;
+};
